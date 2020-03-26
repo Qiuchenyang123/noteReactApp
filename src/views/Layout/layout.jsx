4e7bd9f52670ed4750faa1adf5ba2a01d9287bf2
qcy <617994150@qq.com>
2020-03-26 19:06:56 +0800

    注册页面

--- src/views/Layout/layout.jsx
+++ src/views/Layout/layout.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "../Login/login";
-
+import Register from "../Register/register";
 class Layout extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" component={Login}></Route>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/register" render={() => {
+                        <Register />
+                        }}>
+                    </Route>
                 </Switch>
             </BrowserRouter>
         );
---
+++ src/views/Register/register.jsx
@@ -0,0 +1,173 @@
+import React, {Component} from 'react';
+import {
+    Form,
+    Input,
+    Tooltip,
+    Cascader,
+    Select,
+    Row,
+    Col,
+    Checkbox,
+    Button,
+    AutoComplete,
+} from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+
+        }
+    }
+    formRef = React.createRef();
+    onFinish = values => {
+
+    };
+    render() {
+        const { Option } = Select;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+
+        const prefixSelector = (
+            <Form.Item name="prefix" noStyle>
+                <Select style={{ width: 70 }}>
+                    <Option value="86">+86</Option>
+                    <Option value="87">+87</Option>
+                </Select>
+            </Form.Item>
+        );
+
+        return (
+            <Form
+                {...formItemLayout}
+                form={this.formRef}
+                name="register"
+                onFinish={this.onFinish}
+                initialValues={{
+                    prefix: '86'
+                }}
+                scrollToFirstError
+            >
+                <Form.Item
+                    name="email"
+                    label="邮箱"
+                    rules={[
+                        {
+                            type: 'email',
+                            message: '请输入正确的邮箱地址',
+                        },
+                        {
+                            required: true,
+                            message: '请输入邮箱',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    label="密码"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    label="确认密码"
+                    dependencies={['password']}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please confirm your password!',
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(rule, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject('The two passwords that you entered do not match!');
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="nickname"
+                    label={
+                        <span>
+            昵称&nbsp;
+                            <Tooltip title="What do you want others to call you?">
+              <QuestionCircleOutlined />
+            </Tooltip>
+          </span>
+                    }
+                    rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name="phone"
+                    label="手机号码"
+                    rules={[{ required: true, message: 'Please input your phone number!' }]}
+                >
+                    <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+                </Form.Item>
+
+                <Form.Item
+                    name="agreement"
+                    valuePropName="checked"
+                    rules={[
+                        { validator:(_, value) => value ? Promise.resolve() : Promise.reject('Should accept agreement') },
+                    ]}
+                    {...tailFormItemLayout}
+                >
+                    <Checkbox>
+                        I have read the <a href="">agreement</a>
+                    </Checkbox>
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">
+                        注册
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+
+export default Register;
